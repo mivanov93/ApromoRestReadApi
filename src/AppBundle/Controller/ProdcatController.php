37d9f490b68ac798c232f829d54c43cacf3be39f
x0r <x0r@localhost.localdomain>
2016-07-04 13:20:40 +0300

    prodcat products count

--- src/AppBundle/Controller/ProdcatController.php
+++ src/AppBundle/Controller/ProdcatController.php
@@ -3,7 +3,15 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Prodcat;
+use AppBundle\Entity\Products;
+use AppBundle\Utils\ResponseFactory;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Transliterator\Settings;
+use Transliterator\Transliterator;
+use function mb_strlen;
 
 class ProdcatController extends Controller {
 
@@ -21,6 +29,78 @@ class ProdcatController extends Controller {
         $prodcatList = $qry->getArrayResult();
         $jsonResponseFactory = $this->get('response_factory');
         $r = $jsonResponseFactory->getJsonMysqlRowsResponse($prodcatList, count($prodcatList), self::CACHE_TIME);
+        return $r;
+    }
+
+    public function indexByProdSearchAction(Request $request) {
+        $cached = true;
+        $brandsIds = $request->get('brandsIds');
+        $searchQuery = $request->get('searchQuery');
+        $maxNewPrice = (double) $request->get('maxNewPrice');
+        $repo = $this->getDoctrine()->getRepository(Prodcat::class);
+        /* @var $qb QueryBuilder */
+        $qb = $repo->createQueryBuilder('pc');
+        $qb->select(self::PRODCAT_FIELDS . ',count(p.prodId) as pCount');
+        $qb->leftJoin(Products::class, 'p', Query\Expr\Join::WITH, 'p.prodProdcat = pc.prodcatId');
+        $qb->leftJoin('p.prodPiCollection', 'pi');
+        $qb->innerJoin('pc.prodcatLinkage', 'pl');
+        $qb->groupBy('pc.prodcatId');
+        
+        if ($brandsIds !== null) {
+            $qb->andWhere('p.prodBrand IN (:brandsIds)');
+            $brandsIds = explode(',', $brandsIds, ProductsController::MAX_BRANDS);
+            $qb->setParameter('brandsIds', $brandsIds);
+            $brCount = count($brandsIds);
+            $maxBrCount = $this->get('common_data')->getBrandsCount();
+            if ($brCount > 3 && $brCount < $maxBrCount - 3) {
+                $cached&=false;
+            }
+        }
+        if ($maxNewPrice > 0) {
+            $qb->andWhere('p.prodNewprice <= :maxNewPrice');
+            $qb->setParameter('maxNewPrice', (double) $maxNewPrice);
+        }
+
+        if (floor($maxNewPrice) !== ceil($maxNewPrice)) {
+            $cached&=false;
+        }
+        if (mb_strlen($searchQuery) > 3) {
+            // $searchQuery=preg_replace("/[^[:alnum:][:space:]]/u", '', $searchQuery);
+            $searchQuery = preg_replace('/[^\p{L}\p{N}_]+/u', ' ', $searchQuery);
+            $searchQuery = preg_replace('/[+\-><\(\)~*\"@]+/u', ' ', $searchQuery);
+        }
+        if (mb_strlen($searchQuery) > 3) {
+            $expl = explode(' ', $searchQuery, 5);
+            $transliterator = new Transliterator(Settings::LANG_BG);
+            $newSearchQuery = [];
+            foreach ($expl as $word) {
+                $newSearchQuery[] = $word;
+                $toLatin = $transliterator->cyr2Lat($word);
+                if ($toLatin !== $word) {
+                    $newSearchQuery[] = $toLatin;
+                }
+                $toCyr = $transliterator->lat2Cyr($word);
+                if ($toCyr !== $word) {
+                    $newSearchQuery[] = $toCyr;
+                }
+            }
+
+            $searchQuery = implode(' ', $newSearchQuery);
+            $qb->andWhere("p.prodId is null or (pi.piId is not null and MATCH_AGAINST(p.prodName, p.prodDescr,p.prodKeywords, :searchQuery 'IN BOOLEAN MODE') > 0)");
+            $qb->setParameter('searchQuery', $searchQuery);
+        } else {
+            $qb->andWhere('p.prodId is null or pi.piId is not null');
+        }
+
+        $qry = $qb->getQuery();
+       // echo $qry->getDQL();
+        if ($cached) {
+            $qry->setResultCacheId('products_search');
+            $qry->setResultCacheLifetime(self::CACHE_TIME);
+        }
+        $products = $qry->getArrayResult();
+        $r = $this->get('response_factory')->getJsonMysqlRowsResponse($products, count($products), self::CACHE_TIME,
+                                                                                       $cached ? ResponseFactory::publicCache : ResponseFactory::privateCache);
         return $r;
     }
 
